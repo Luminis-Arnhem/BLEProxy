8c12887d069a965f183e8582147c3715592608d5
Tom Hanekamp <tom.hanekamp@luminis.eu>
2021-04-07 15:18:55 +0200

    Created a new empty MacOS app project.

---
+++ BLEProxy/BLEProxy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BLEProxy
+//
+//  Created by Tom Hanekamp on 07/04/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BLEProxy/BLEProxy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BLEProxy
+//
+//  Created by Tom Hanekamp on 07/04/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
