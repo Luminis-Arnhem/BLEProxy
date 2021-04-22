3c4f3a89c141f90cbc8aa143aaa0d3296d95f6f2
Tom Hanekamp <tom.hanekamp@luminis.eu>
2021-04-22 17:19:51 +0200

    Added BLEPeripheral to ViewController.

--- BLEProxy/BLEProxy/ViewController.swift
+++ BLEProxy/BLEProxy/ViewController.swift
@@ -1,19 +1,23 @@
 import Cocoa
 import CoreBluetooth
 
-class ViewController: NSViewController, BleCentralDelegate {
+class ViewController: NSViewController, BleCentralDelegate, BlePeripheralDelegate {
     
     @IBOutlet weak var startProxyButton: NSButton!
     @IBOutlet weak var stopProxyButton: NSButton!
     @IBOutlet weak var logView: NSTextField!
     
     var bleCentral: BleCentral?
+    var blePeripheral: BlePeripheral?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.bleCentral = BleCentral()
         self.bleCentral?.delegate = self
+        
+        self.blePeripheral = BlePeripheral()
+        self.blePeripheral?.delegate = self
     }
 
     @IBAction func startProxy(_ sender: Any) {
@@ -34,18 +38,50 @@ class ViewController: NSViewController, BleCentralDelegate {
     
     func connected(services: [BleService]) {
         log("Connected to peripheral")
+        self.blePeripheral?.startAdvertising(services: services)
     }
     
     func disconnected(reason: String) {
         log("Stopped because: \(reason)")
+        self.blePeripheral?.stopAdvertising()
     }
     
-    func dataWritten(onCharacteristicWithUUID: CBUUID, withResult: CBATTError.Code) {
-        log("Data written from central to peripheral on characteristic \(onCharacteristicWithUUID.uuidString) with result: \(withResult)")
+    func advertisingStarted() {
+        log("Started advertising peripheral")
     }
     
-    func dataReceived(data: Data, onCharacteristicWithUUID: CBUUID) {
-        log("Received \(data) from peripheral to pass to central on characteristic \(onCharacteristicWithUUID.uuidString)")
+    func advertisingStopped(reason: String) {
+        log("Stopped advertising because: \(reason)")
+    }
+    
+    func read(fromCharacteristicUUID uuid: CBUUID) {
+        log("Reading data from characteristic \(uuid.uuidString)")
+        self.bleCentral?.readData(characteristicUUID: uuid)
+    }
+    
+    func write(data: Data, toCharacteristicUUID uuid: CBUUID) {
+        log("Writing data \(data) from central to peripheral on characteristic \(uuid.uuidString)")
+        self.bleCentral?.writeData(characteristicUUID: uuid, data: data, writeType: .withResponse)
+    }
+    
+    func registerForNotifications(onCharacteristicWithUUID uuid: CBUUID) {
+        log("Registering for notifications on characteristic \(uuid.uuidString)")
+        self.bleCentral?.registerForNotifications(characteristicUUID: uuid)
+    }
+    
+    func unregisterFromNotifications(onCharacteristicWithUUID uuid: CBUUID) {
+        log("Unregistering from notifications on characteristic \(uuid.uuidString)")
+        self.bleCentral?.unregisterFromNotifications(characteristicUUID: uuid)
+    }
+    
+    func dataWritten(onCharacteristicWithUUID uuid: CBUUID, withResult result: CBATTError.Code) {
+        log("Data written from central to peripheral on characteristic \(uuid.uuidString) with result: \(result)")
+        self.blePeripheral?.confirmWriteRequest(onCharacteristicUUID: uuid, withResult: result)
+    }
+    
+    func dataReceived(data: Data, onCharacteristicWithUUID uuid: CBUUID) {
+        log("Received \(data) from peripheral to pass to central on characteristic \(uuid.uuidString)")
+        self.blePeripheral?.dataReceived(data: data, onCharacteristicUUID: uuid)
     }
     
     func logMessage(message: String) {
